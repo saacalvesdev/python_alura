b112375a26028c31448a682454add747cc7684c7
saacalvesdev <saacalves.dev@gmail.com>
2022-11-21 19:26:49 -0300

    Criando aquivo de gerenciamento dos jogos, e estabelecer as importações.

---
+++ Jogos/jogos.py
@@ -0,0 +1,11 @@
+print("*" * 33)
+print("Bem Vindo ao Jogo de Forca!")
+print("*" * 33)
+
+print('(1) Forca (2) Adivinhação')
+jogo = input (int("Escolha seu Jogo: "))
+
+if (jogo == 1):
+    print('Iniciando o jogo da Forca...')
+elif (jogo == 2):
+    print('Iniciando o jogo de Adivinhação...')
