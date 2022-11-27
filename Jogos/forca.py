1c9d93da2d184766b0c9124417df0aefeb57721f
saacalvesdev <saacalves.dev@gmail.com>
2022-11-27 19:25:49 -0300

    Foram implementadas as configurações de tratamento de string e o laço de repetição.

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,8 +1,25 @@
-
 def jogar():
     print("*" * 33)
     print("Bem Vindo ao Jogo de Forca!")
     print("*" * 33)
+    
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute = input('Qual a letra? \n')
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print('Encontrei a letrab {} na posição {}.'.format(letra, index))
+            index = index + 1
+            
+        print('Jogando')
 
     print("Fim do Jogo!!!")
 
